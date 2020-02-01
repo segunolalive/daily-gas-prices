b3736f1e92a56e544bd8a17108443a4613242e19
Segun <segunolalive@gmail.com>
2020-02-01 09:11:19 +0100

    create daily gas prices CSV

---
+++ README.md
@@ -0,0 +1,54 @@
+# Data Wrangler Script
+
+## Introduction
+
+This is a simple script for generating daily and monthy gas prices from Henry Hub gas prices
+
+## Data source
+
+[Henry Hub gas prices](http://www.eia.gov/dnav/ng/hist/rngwhhdm.htm)
+
+## Table of Contents
+
+1. [Tech Stack Used](/tech-stack-used/)
+2. [Dependencies](/dependencies/)
+3. [How To Use](/how-to-use/)
+4. [Author](/author/)
+5. [License](/license/)
+
+## Technologies Used
+
+- [Python3](https://www.python.org/downloads/)
+
+## Dependencies
+
+- [requests](https://requests.readthedocs.io/en/master/)
+- [beautifulsoup4](https://pypi.org/project/beautifulsoup4/)
+
+## How To Use
+
+To clone and run this application, you'll need [Git](https://git-scm.com) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+git clone https://github.com/segunolalive/daily-gas-prices.git
+
+# Go into the repository
+cd daily-gas-prices
+
+# Install dependencies
+pip3 install
+
+# Run the script
+python3 gas_prices.py
+```
+
+## Author
+
+Adebisi Olusegun Ola
+
+## License
+
+MIT
+
+---
---
+++ gas_price.py
@@ -0,0 +1,107 @@
+import os
+import csv
+import logging
+import traceback
+import datetime
+
+import requests
+from bs4 import BeautifulSoup
+
+class GasPrice:
+    """
+    a suite of utilities to generate csv version of daily gas prices from www.eia.gov
+    """
+    URL = 'https://www.eia.gov/dnav/ng/hist/rngwhhdD.htm'
+    OUTPUT_DIR = 'output/'
+    DAILY_PRICES = OUTPUT_DIR + 'daily_prices.csv'
+    MONTHLY_PRICES = OUTPUT_DIR + 'monthly_prices.csv'
+    html = None
+    data = None
+    formatted_data = []
+
+    def get_daily_prices_csv(self):
+        """
+        write CSV file containing cleaned gas prices and corresponding dates
+        """
+        self.__get_page().__extract_rows().__format_prices()
+
+        directory = os.path.join(os.getcwd(), self.OUTPUT_DIR)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        with open(self.DAILY_PRICES, 'w') as csv_file:
+            csv_file.write('DATE, PRICE \n')
+            for day in self.formatted_data:
+                csv_file.write(','.join(day))
+                csv_file.write('\n')
+
+        logging.info('SUCCESS! Daily Gas Prices Generated at {0}'.format(
+                     os.path.join(os.getcwd(), self.DAILY_PRICES)))
+
+    def __get_page(self):
+        """
+        method for fetching the gas prices html page
+        """
+        try:
+            req = requests.get(self.URL)
+            if req.status_code == 200:
+                self.html = req.content
+                logging.info('HTML page fetched successfully...')
+                return self
+            else:
+                message = 'ERROR. Server returned a status code of ' + \
+                    str(req.status_code)
+                logging.warning(message)
+        except Exception as e:
+            message = 'An Error occured while fetching the html page. ' + \
+                str(e)
+            logging.error(message)
+            logging.error(traceback.format_exc())
+
+    def __extract_rows(self):
+        data = {'dates': [], 'prices': []}
+        summary = 'Henry Hub Natural Gas Spot Price (Dollars per Million Btu)'
+        soup = BeautifulSoup(self.html, 'html.parser')
+        table = soup.find(summary=summary)
+        table_rows = table.find_all('tr')
+        for row in table_rows:
+            date_cell = row.find('td', {'class': 'B6'})
+            price_cells = row.find_all('td', {'class': 'B3'})
+            if date_cell:
+                data.get('dates').append(date_cell)
+            if len(price_cells):
+                data.get('prices').append(price_cells)
+        self.data = data
+        return self
+
+    def __format_prices(self):
+        dates = self.data.get('dates')
+        prices = self.data.get('prices')
+        for i, date in enumerate(dates):
+            start_date = self.__get_start_date(date)
+            day_offset = 0
+            for price in prices[i]:
+                current_date = start_date + datetime.timedelta(days=day_offset)
+                current_date = current_date.strftime('%Y-%b-%d')
+                self.formatted_data.append([current_date, price.text])
+                day_offset += 1
+        return self
+
+    def __get_start_date(self, string):
+        start_date = string.text.replace('\xa0\xa0', '').replace(
+            '- ', ' ').replace('-', ' ').split(' ')[0:3]
+        return self.normalise_date(start_date)
+
+    def normalise_date(self, string):
+        return datetime.datetime.strptime('-'.join(string), '%Y-%b-%d')
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    gas_prices = GasPrice()
+    gas_prices.get_daily_prices_csv()
+
+
+if __name__ == '__main__':
+    main()
